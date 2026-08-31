a0deaf3b47aa3251382b66629b4a06355150d048
kishan-rai2912 <raikishan2912@gmail.com>
2026-08-31 12:16:12 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import First from './First'
-import MyApp from './First';
-import MyButton from './First'
-import Class1 from './Class1';
-import C2f from './C2f';
+
 import Header from './Header';
 import Footer from './Footer';
 import Main from './Main';
-import reportWebVitals from './reportWebVitals';
+
 import {BrowserRouter ,Routes, Route} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
